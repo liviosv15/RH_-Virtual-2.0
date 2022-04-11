756a1626ebc2fed7e1e41d09209b310ad32f765e
anaclarav <51497041+anaclarav@users.noreply.github.com>
2022-04-11 14:05:02 -0300

    Delete RH_virtual directory

--- RH_virtual/pessoas/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Pessoas
-
-# Register your models here.
-admin.site.register(Pessoas)
--- RH_virtual/pessoas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import Pessoas 
-
-class cadastroPessoa(forms.ModelForm):
-    class Meta:
-        model = Pessoas 
-        fields = ('nome', 'sobrenome','email_login', 'senha_login')
